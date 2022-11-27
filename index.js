cf8842ac363f18a2555e10adba11d71bf1cb82e9
Cesar Santos <cesarsantossouza99@gmail.com>
2022-11-27 20:00:30 -0300

    Projeto Node.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const uuid = require('uuid') 
+const cors = require ('cors')
+const porta = 3001
+const app = express() 
+app.use(cors())
+
+app.use(express.json())
+
+
+const users = []
+
+
+const checkUsersId = (request, response, next) => {
+    const {id} = request.params
+
+    const index = users.findIndex((user) => user.id !== id)
+
+    if (index < 0) {
+        return response.status(404).json({message: "User not found"})
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next()
+}
+
+
+app.get('/users', (request, response) => {
+    return response.json(users) 
+})
+
+
+app.post('/users', (request, response) => {
+    const {name, age} = request.body
+    const user = {id:uuid.v4(), name, age}
+    users.push(user)
+    return response.status(201).json(user) 
+})
+
+app.put('/users/:id', checkUsersId, (request, response) => {
+    const {name, age} = request.body
+    const index = request.userIndex
+    const id = request.usetId
+
+    const updatedUser = {id, name, age}
+
+    users[index] = updatedUser
+
+    return response.json(updatedUser) 
+})
+
+app.delete('/users/:id', checkUsersId, (request, response) => {
+    const index = request.userIndex
+
+    users.splice(index,1)
+
+    return response.status(204).json() 
+})
+
+
+
+
+app.listen(porta, () => {
+    console.log( 'Olá');
+})
+
